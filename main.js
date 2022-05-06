aecbc6b881d05ed2db1732d4bdee8447cdf97acc
ViolaterZ <73651672+ViolaterZ@users.noreply.github.com>
2022-05-06 19:44:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -25,11 +25,31 @@ document.head.insertAdjacentHTML('afterbegin', meta_HTML);
 app.insertAdjacentHTML('afterbegin', header_HTML);
 
 data.forEach((client) => {
-    const version = client.version.toLowerCase().split(' ').join('-')
+    const version = client.version.toLowerCase().split(' ').join('-');
+    const formatDate = function (date, locale) {
+        const calcDaysPassed = (date1, date2) =>
+        Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+      
+        const daysPassed = calcDaysPassed(new Date(), date);
+        console.log(daysPassed);
+      
+        if (daysPassed === 0) return 'Today';
+        if (daysPassed === 1) return 'Yesterday';
+        if (daysPassed <= 7) return `${daysPassed} days ago`;
+      
+        // const day = `${date.getDate()}`.padStart(2, 0);
+        // const month = `${date.getMonth() + 1}`.padStart(2, 0);
+        // const year = date.getFullYear();
+        // return `${day}/${month}/${year}`;
+        
+        return new Intl.DateTimeFormat(locale).format(date);
+    };
+
     const list_HTML = `
     <div class="download-item">
         <i class="fa-solid fa-file-zipper file-icon"></i>
         <span class="span-version">${client.name} ${client.version}</span>
+        <span class="span-updated">Last Updated ${formatDate(new Date(client.lastUpdated), 'en-US')}</span>
         <span class="span-size">${client.fileSize}</span>
         <button class="button-download" data-version="${version}" ${!client.published ? 'disabled' : ''} ${!client.published ? 'title="Not yet released!"' : ''}>
             <i class="fa-solid fa-cloud-arrow-down"></i> Download
@@ -45,3 +65,5 @@ data.forEach((client) => {
 });
 
 app.querySelector('.download-container').insertAdjacentHTML('beforeend', `<span class="span-credit">Powered by <i class="fa-brands fa-dropbox"></i> Dropbox</span>`);
+
+console.log(new Date().toISOString())
