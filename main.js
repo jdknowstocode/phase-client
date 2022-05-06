9cb03a7bbe0f9b647f457c457fe115fd990596d4
ViolaterZ <73651672+ViolaterZ@users.noreply.github.com>
2022-05-06 15:01:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const app = document.querySelector('.app');
 const header_HTML = `
 <div class="img-container">
     <img src="${logoSrc}" alt="" class="logo">
-    <span class="span-version">${data[0].published ? data[0].name : data[1].name} ${data[0].version ? data[0].version : data[1].version}</span>
+    <span class="span-version">${data[0].published ? data[0].name : data[1].name} ${data[0].published ? data[0].version : data[1].version}</span>
 </div>
 `;
 
